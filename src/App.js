1f9872c81356543a0d244cb45772304a4bf75cfb
Kxyaaad <823942471@qq.com>
2019-09-26 17:35:32 +0800

    ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
+import {Button, message} from 'antd'
 import './App.css';
 
 
 export default class App extends Component {
-
+  handleClick = () => {
+    message.success('成功了！')
+  }
   render() {
-    return <div>App</div>
+    return <Button type="primary" onClick={this.handleClick}>Primary</Button>
   }
 }
