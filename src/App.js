98c4bc913b50122e5eb35adefa329c5f5a6d86c7
Kxyaaad <823942471@qq.com>
2019-09-29 11:23:52 +0800

    加入路由，但是好像不起作用

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
-import {Button, message} from 'antd'
+import admin from './pages/admin/Admin'
+import Login from './pages/login/login'
+import { BrowserRouter,Route, Switch } from 'react-router-dom'
 import './App.css';
 
 
+
 export default class App extends Component {
-  handleClick = () => {
-    message.success('成功了！')
-  }
+  
   render() {
-    return <Button type="primary" onClick={this.handleClick}>Primary</Button>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' Component={admin} exact ></Route>
+          <Route path='/login' Component={Login}></Route>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './login.less'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className='login'>
+                <header className='login-header'>dth</header>
+                <section classNam='login-content'>ht</section>
+            </div>
+
+        )
+    }
+}
